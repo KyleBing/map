e536ba664c4e5049c8edf67918c97aaa14577131
Kyle Bing <kylebing@163.com>
2021-07-09 15:13:40 +0800

    + 代码缩进优化

--- src/page/route/lines.js
+++ src/page/route/lines.js
@@ -153,15 +153,15 @@ const LINES = [
 
 ]
 const COLORS = {
-    green: "#4CD964",
-    syan: "#5AC8FA",
-    blue: "#007AFF",
-    purple: "#5856D6",
+    green  : "#4CD964",
+    cyan   : "#5AC8FA",
+    blue   : "#007AFF",
+    purple : "#5856D6",
     magenta: "#FF2D70",
-    red: "#FF3B30",
-    orange: "#FF9500",
-    yellow: "#FFCC00",
-    gray: "#8E8E93",
+    red    : "#FF3B30",
+    orange : "#FF9500",
+    yellow : "#FFCC00",
+    gray   : "#8E8E93",
 }
 export default {
     COLORS, LINES
