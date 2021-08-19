864380502b26217054b664e486cc0e0839fd45f3
邴新科 <kylebing@163.com>
2021-08-19 17:30:35 +0800

    - 路径推荐中的自身定位

--- src/page/route/lines.vue
+++ src/page/route/lines.vue
@@ -41,9 +41,8 @@ export default {
             key: "581591b581149549d9035d039e83e368", // 开发应用的 ID
             version: "2.0",   // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
             plugins: [
-                // 'AMap.ToolBar', // 缩放按钮
+                'AMap.ToolBar', // 缩放按钮
                 'AMap.Scale', // 比例尺
-                'AMap.Geolocation', // 定位
                 'AMap.DragRoute', // 定位
             ],
 
@@ -54,24 +53,8 @@ export default {
                 zoom: 11
             })
 
-            // this.map.addControl(new AMap.ToolBar())
+            this.map.addControl(new AMap.ToolBar())
             this.map.addControl(new AMap.Scale())
-
-
-            // 定位
-            let geolocation = new AMap.Geolocation({
-                // 是否使用高精度定位，默认：true
-                enableHighAccuracy: true,
-                // 设置定位超时时间，默认：无穷大
-                timeout: 10000,
-                // 定位按钮的停靠位置的偏移量，默认：Pixel(10, 20)
-                buttonOffset: new AMap.Pixel(10, 20),
-                //  定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-                zoomToAccuracy: true,
-                //  定位按钮的排放位置,  RB表示右下
-                buttonPosition: 'RB'
-            })
-            geolocation.getCurrentPosition(this.setMapCenterToUserLocation)
 
             // 获取 Route 中的路线 ID
             this.activeLineObj = this.lines[parseInt(this.$route.params.lineId) - 1]
