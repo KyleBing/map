c0bf0e6bd99e6efd60b979cc80ba3d81fb4e7848
邴新科 <kylebing@163.com>
2022-07-05 14:32:44 +0800

    修改主页动画

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -12,11 +12,9 @@ import {mapState} from "vuex"
 import mapConfig from "../../mapConfig";
 
 const POSITION = {
-    hanyu: [117.148734, 36.659771],
-    home: [117.119074, 36.675504],
-    cityCenter: [117.120114, 36.652366],
-    cityCenterEast: [117.12492, 36.652189],
-    yinHe: [117.12972, 36.67326]
+    daMingHu: [117.026217, 36.670921], // 大明湖
+    quanChengGuangChang: [117.021226, 36.661487], // 泉城广场
+    baoTuQuan: [117.015871, 36.661057], // 趵突泉
 }
 let AMap = null
 
@@ -53,7 +51,7 @@ export default {
             this.map = new AMap.Map('container', {
                 viewMode: '3D',
                 zoom: 17,
-                center: POSITION.yinHe,
+                center: POSITION.daMingHu,
                 // mapStyle: 'amap://styles/45311ae996a8bea0da10ad5151f72979',
                 showBuildingBlock: true, // 显示建筑物
                 showLabel: false, // 不显示地名什么的
@@ -83,8 +81,8 @@ export default {
 
             this.loca.viewControl.addAnimates([{
 /*                center: {
-                    value: POSITION.cityCenter, // 动画终点的经纬度
-                    control: [POSITION.cityCenterEast, POSITION.cityCenter], // 过渡中的轨迹控制点，地图上的经纬度
+                    value: POSITION.quanChengGuangChang, // 动画终点的经纬度
+                    control: [POSITION.quanChengGuangChangEast, POSITION.quanChengGuangChang], // 过渡中的轨迹控制点，地图上的经纬度
                     timing: [0.42, 0, 0.4, 1], // 动画时间控制点
                     duration: 2000, // 过渡时间，毫秒（ms）
                 },*/
@@ -92,7 +90,7 @@ export default {
                 pitch: {
                     value: 45, // 动画终点的俯仰角度
                     control: [[0, -60], [1, 45]], // 控制器，x是0～1的起始区间，y是pitch值
-                    timing: [0, 0, 1, 1], // 这个值是线性过渡
+                    timing: [0.5, 0.2, 0.5, 0.8], // 这个值是线性过渡
                     duration: 8000,
                 },
 /*                // 缩放等级动画
@@ -104,16 +102,16 @@ export default {
                 },*/
                 // 旋转动画
                 rotation: {
-                    value: 120, // 动画终点的地图旋转角度
-                    control: [[0, 0], [1, 120]], // 控制器，x是0～1的起始区间，y是rotation值
+                    value: 90, // 动画终点的地图旋转角度
+                    control: [[0, 0], [1, 90]], // 控制器，x是0～1的起始区间，y是rotation值
                     timing: [0, 0, 1, 1],
                     duration: 8000,
                 }
             }],()=>{
                 this.loca.viewControl.addAnimates([{
                     center: {
-                        value: POSITION.cityCenter, // 动画终点的经纬度
-                        control: [POSITION.yinHe, POSITION.cityCenter], // 过渡中的轨迹控制点，地图上的经纬度
+                        value: POSITION.quanChengGuangChang, // 动画终点的经纬度
+                        control: [POSITION.daMingHu, POSITION.quanChengGuangChang], // 过渡中的轨迹控制点，地图上的经纬度
                         timing: [0, 0, 1, 1], // 动画时间控制点
                         duration: 8000, // 过渡时间，毫秒（ms）
                     },
@@ -121,25 +119,19 @@ export default {
                 }],()=>{
                     this.loca.viewControl.addAnimates([{
                         center: {
-                            value: POSITION.yinHe, // 动画终点的经纬度
-                            control: [POSITION.cityCenter, POSITION.yinHe], // 过渡中的轨迹控制点，地图上的经纬度
-                            timing: [0, 0, 1, 1], // 动画时间控制点
+                            value: POSITION.daMingHu, // 动画终点的经纬度
+                            control: [POSITION.quanChengGuangChang, POSITION.daMingHu], // 过渡中的轨迹控制点，地图上的经纬度
+                            timing: [0.5, 0.2, 0.5, 0.8], // 这个值是线性过渡
                             duration: 8000, // 过渡时间，毫秒（ms）
                         },
-                    }],()=>{
-                        this.loca.viewControl.addAnimates([{
-                            // 旋转动画
-                            rotation: {
-                                value: 180, // 动画终点的地图旋转角度
-                                control: [[0, 120], [1, 180]], // 控制器，x是0～1的起始区间，y是rotation值
-                                timing: [0, 0, 1, 1],
-                                duration: 8000,
-                            }
-                        }],()=>{
-
-                        })
-
-                    })
+                        // 旋转动画
+                        rotation: {
+                            value: 0, // 动画终点的地图旋转角度
+                            control: [[0, -90], [1, 0]], // 控制器，x是0～1的起始区间，y是rotation值
+                            timing: [0.5, 0.2, 0.5, 0.8], // 这个值是线性过渡
+                            duration: 8000,
+                        }
+                    }])
                 })
 
             })
