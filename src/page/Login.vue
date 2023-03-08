8409b903888d782ceb69120457a215e5efe987d6
KyleBing(MBP14) <kylebing@163.com>
2023-03-09 00:11:00 +0800

    login bg heart size adjustment

--- src/page/Login.vue
+++ src/page/Login.vue
@@ -53,7 +53,7 @@ export default {
     },
     mounted() {
         this.height = innerHeight
-        this.animatedBg = new AnimateHeartCanvas(0, 360, 150, 10, 50, '#3d3d3d')
+        this.animatedBg = new AnimateHeartCanvas(0, 360, 250, 20, 50, '#3d3d3d')
     },
     beforeDestroy() {
         this.animatedBg.destroy()
