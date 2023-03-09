572384fb5095b000789bffb7c39d891eff091ba2
邴新科-工作 <kylebing@163.com>
2023-03-09 11:19:00 +0800

    fix api path

--- src/api/routeApi.js
+++ src/api/routeApi.js
@@ -1,8 +1,8 @@
 import request from './request'
 export default {
-    add( requestData){return request('post', null, requestData, false, null, '/map-route/add')},
-    list(requestData){return request('post', null, requestData, false,null,'/map-route/list')},
-    modify(requestData){return request('put', null, requestData, false, null,'/map-route/modify')},
-    delete(requestData){return request('delete', null, requestData, false, null, '/map-route/delete')},
-    detail(params){return request('get', params, null, false, null, '/map-route/detail')},
+    add( requestData){return request('post', null, requestData, false, null, 'map-route/add')},
+    list(requestData){return request('post', null, requestData, false,null,'map-route/list')},
+    modify(requestData){return request('put', null, requestData, false, null,'map-route/modify')},
+    delete(requestData){return request('delete', null, requestData, false, null, 'map-route/delete')},
+    detail(params){return request('get', params, null, false, null, 'map-route/detail')},
 }
--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,14 +1,14 @@
 import request from "./request";
 
 export default {
-    login(requestData) {return request('post', null, requestData, false, null, '/user/login')},
-    register(requestData) {return request('post', {}, requestData, false, null, '/user/register')},
+    login(requestData) {return request('post', null, requestData, false, null, 'user/login')},
+    register(requestData) {return request('post', {}, requestData, false, null, 'user/register')},
 
 
-    add(requestData) {return request('post', {}, requestData, false, null, '/user/add')},
-    delete(requestData) {return request('delete', {}, requestData, false, null, '/user/delete')},
-    modify(requestData) {return request('put', {}, requestData, false, null, '/user/modify')},
-    detail(params) {return request('get', params, null, false, null, '/user/detail')},
-    list(requestData) {return request('post', null, requestData, false, null, '/user/list')},
-    changePassword(requestData) {return request('put', null, requestData, false, null, '/user/change-password')},
+    add(requestData) {return request('post', {}, requestData, false, null, 'user/add')},
+    delete(requestData) {return request('delete', {}, requestData, false, null, 'user/delete')},
+    modify(requestData) {return request('put', {}, requestData, false, null, 'user/modify')},
+    detail(params) {return request('get', params, null, false, null, 'user/detail')},
+    list(requestData) {return request('post', null, requestData, false, null, 'user/list')},
+    changePassword(requestData) {return request('put', null, requestData, false, null, 'user/change-password')},
 }
