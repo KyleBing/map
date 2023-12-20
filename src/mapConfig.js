6c12dba373fc9c5d5ca203bd12126d94f43b8f70
KyleBing <kylebing@163.com>
2023-12-20 11:38:36 +0800

    renew mapConfig, ready to public

---
+++ src/mapConfig.js
@@ -0,0 +1,14 @@
+export default {
+    key_web_js: '', // web js key
+    key_service: '',  // web服务 key
+    // 管理地址在：https://console.amap.com/dev/key/app
+
+    // 七牛云 与 《标题日记》共用一个 仓库
+    // 地址： https://portal.qiniu.com/kodo/overview
+    qiniu_img_base_url: 'http://apple-image.kylebing.cn/', // 空间域名，最后面带 `/`
+    qiniu_bucket_name: 'apple-image', // 七牛云对象存储空间的名称
+    thumbnail200_suffix: 'thumbnail_200px', // 七牛云缩略图样式名  200x200px 质量75
+    thumbnail600_suffix: 'thumbnail_600px', // 七牛云缩略图样式名  600x600px 质量75
+    thumbnail1000_suffix: 'thumbnail_1000px', // 七牛云缩略图样式名  1000x1000px 质量75
+    thumbnail1500_suffix: 'thumbnail_1500px', // 七牛云缩略图样式名  1500x1500px 质量75
+}
