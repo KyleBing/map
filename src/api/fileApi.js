f1abca61f3c76833ac5d6495c4c0d9c3757a67da
KyleBing(MBP14) <kylebing@163.com>
2024-02-15 20:34:24 +0800

    fix map image upload error

--- src/api/fileApi.js
+++ src/api/fileApi.js
@@ -1,5 +1,5 @@
 import request from "./request";
 
 export default {
-    getUploadToken(params) {return request('get', params, null,false, null,  '/file/')},
+    getUploadToken(params) {return request('get', params, null,false, null,  '/image-qiniu/')},
 }
